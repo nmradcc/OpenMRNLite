5f41a237a194ccf173ef66795bbfd81c6bbd73a5
David Vescovi <dvescovi@tampabay.rr.com>
2025-12-18 14:56:18 -0500

    debugable

--- examples/STM32H563_Nucleo_FR/Inc/main.h
+++ examples/STM32H563_Nucleo_FR/Inc/main.h
@@ -39,7 +39,8 @@ extern "C" {
 
 /* Exported types ------------------------------------------------------------*/
 /* USER CODE BEGIN ET */
-
+extern FDCAN_HandleTypeDef hfdcan1;
+extern FDCAN_HandleTypeDef hfdcan2;
 /* USER CODE END ET */
 
 /* Exported constants --------------------------------------------------------*/
